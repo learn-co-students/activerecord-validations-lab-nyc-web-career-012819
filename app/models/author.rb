be63feb1c5bad5ffe6556e670efd75e8a3e2c7a1
github username <github email address>
2019-02-25 18:05:50 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :phone_number, length: {is: 10 }
+  validates :phone_number, numericality: { only_integer: true, message: "Numbers only!"}
+  validates :name, uniqueness: true
+
 end
