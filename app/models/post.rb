9c4307ff951d8bea0bf2887f084400d0023d1361
Ethan Nam <ethan.s.nam@gmail.com>
2019-02-24 22:41:54 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validate :not_clickbait
+
+  CLICKBAIT_PHRASES = [
+    /Won't Believe/,
+    /Secret/,
+    /Top\d*/,
+    /Guess/
+  ]
+
+  def not_clickbait
+    if CLICKBAIT_PHRASES.none? { |phrase| phrase.match self.title }
+      errors.add(:title, "Clickbait detected")
+    end
+  end
 end
