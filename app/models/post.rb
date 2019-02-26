acd6f58cf224124d8aef9b2e41c9ddb9a878ef54
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-26 07:18:30 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+  # this constant holds an array of 4 Regex patterns, the i stands for case insensative
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+    # #none? checks if each item evalutes to false in a given block.
+    # match checks if the given Regex pattern can be found in the given variable
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
